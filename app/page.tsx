333937c0d2c1f85457a9c6f212c1aee9d4cc161e
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-01-16 14:09:16 +0300

    Theming and setting rules

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-
-
 const Home = () => {
   return (
-    <h1 className="text-blue-100 font-bold text-4xl flex justify-center m-6 max-sm:text-2xl lg:text-6xl">
+    <h1 className="h1-bold max-sm:text-2xl flex justify-center text-blue-700 m-6">
       Welcome to the world of Next.jsssss
     </h1>
   );
-}
+};
 
 export default Home;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,67 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  {
+    plugins: {
+      import: require("eslint-plugin-import"),
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin", // Built-in types are first
+            "external", // External libraries
+            "internal", // Internal modules
+            ["parent", "sibling"], // Parent and sibling types can be mingled together
+            "index", // Then the index file
+            "object", // Object imports
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+    ignores: ["components/ui/**"],
+    overrides: [
+      {
+        files: ["*.ts", "*.tsx"],
+        rules: {
+          "no-undef": "off",
+        },
+      },
+    ],
+  },
 ];
 
-export default eslintConfig;
+export default config;
